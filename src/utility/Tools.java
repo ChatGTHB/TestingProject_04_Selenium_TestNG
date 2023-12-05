fc0113937c9662664ce3a3145890f23fa9ab2d43
ChatGTHB <cihat.kose@hotmail.com>
2023-12-05 12:25:17 +0100

    Updated Tools.java

--- src/utility/Tools.java
+++ src/utility/Tools.java
@@ -1,15 +1,13 @@
 package utility;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 
 public class Tools {
 
-
     public static void wait(int sn) {
         try {
-            Thread.sleep(1000 * sn); // ms beklediği
+            Thread.sleep(1000L * sn);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -19,5 +17,4 @@ public class Tools {
         WebElement messageLabel = BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
         Assert.assertTrue(messageLabel.getText().toLowerCase().contains("success"));
     }
-
 }
