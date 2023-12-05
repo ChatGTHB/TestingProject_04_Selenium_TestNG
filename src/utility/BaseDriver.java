7a30c5540ab2f6122f9cb41b7ceb871c25b94193
ChatGTHB <cihat.kose@hotmail.com>
2023-12-05 12:27:51 +0100

    Edited BaseDriver.java

--- src/utility/BaseDriver.java
+++ src/utility/BaseDriver.java
@@ -3,15 +3,11 @@ package utility;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 
 import java.time.Duration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class BaseDriver {
     public static WebDriver driver;
